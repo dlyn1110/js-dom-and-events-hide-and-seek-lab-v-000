5481fa9335ebdaa9951ca8e7f453bf0ecc5cefbf
Dani Lyn <dlyn1110@gmail.com>
2019-02-16 21:54:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML, 10) + n
+  }
+}
